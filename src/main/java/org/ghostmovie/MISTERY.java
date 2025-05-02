07dab7b34d8cc90f9fcaecb4161e4419e08f35bf
kanishma99 <kanishma.kani99@gmail.com>
2025-05-02 15:55:37 +0530

    create new project

---
+++ src/main/java/org/ghostmovie/MISTERY.java
@@ -0,0 +1,18 @@
+package org.ghostmovie;
+
+public class MISTERY {
+	private void jhh() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void uot() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void wetry() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/main/java/org/ghostmovie/RAILWAY.java
@@ -0,0 +1,18 @@
+package org.ghostmovie;
+
+public class RAILWAY {
+	private void nln() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void SafeVarargs() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void vncvhfy() {
+		// TODO Auto-generated method stub
+
+	}
+}
